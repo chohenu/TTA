cd3a9484179feeb35c67eb1cea1923e057234cdb
chanminpark <pcmin03@gmail.com>
2023-11-10 13:34:45 +0900

    Update vit code

--- hwc_TTA/classifier.py
+++ hwc_TTA/classifier.py
@@ -2,27 +2,48 @@ import logging
 import torch
 import torch.nn as nn
 import torchvision.models as models
-
+import timm
 
 class Classifier(nn.Module):
     def __init__(self, args, train_target=False, checkpoint_path=None):
         super().__init__()
         self.args = args
         model = None
+        self.KNOWN_MODELS = {
+                'ViT-B16': 'vit_base_patch16_224_in21k', 
+                'ViT-B32': 'vit_base_patch32_224_in21k',
+                'ViT-L16': 'vit_large_patch16_224_in21k',
+                'ViT-L32': 'vit_large_patch32_224_in21k',
+                'ViT-H14': 'vit_huge_patch14_224_in21k',
+                'Deit-B16':'deit_base_patch16_224.fb_in1k'
+            }
+        self.FEAT_DIM = {
+                'ViT-B16': 768, 
+                'ViT-B32': 768,
+                'ViT-L16': 1024,
+                'ViT-L32': 1024,
+                'ViT-H14': 1280,
+                'Deit-B16': 768,
 
+            }    
         # 1) ResNet backbone (up to penultimate layer)
-        if not self.use_bottleneck:
+        if not self.use_bottleneck: 
             model = models.__dict__[args.arch](pretrained=True)
             modules = list(model.children())[:-1]
             self.encoder = nn.Sequential(*modules)
             self._output_dim = model.fc.in_features
+
         # 2) ResNet backbone + bottlenck (last fc as bottleneck)
         else:
-            model = models.__dict__[args.arch](pretrained=True)
-            model.fc = nn.Linear(model.fc.in_features, args.bottleneck_dim)
-            bn = nn.BatchNorm1d(args.bottleneck_dim)
-            self.encoder = nn.Sequential(model, bn)
-            self._output_dim = args.bottleneck_dim
+            if 'ViT' in args.arch or 'Deit' in args.arch: 
+                self.encoder = timm.create_model(self.KNOWN_MODELS[args.arch],pretrained=True,num_classes=0)
+                self._output_dim = self.FEAT_DIM[args.arch]
+            else:
+                model = models.__dict__[args.arch](pretrained=True)
+                model.fc = nn.Linear(model.fc.in_features, args.bottleneck_dim)
+                bn = nn.BatchNorm1d(args.bottleneck_dim)
+                self.encoder = nn.Sequential(model, bn)
+                self._output_dim = args.bottleneck_dim
 
         self.fc = nn.Linear(self.output_dim, args.num_classes)
         
